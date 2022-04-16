a13e46eb505c86dafe5ee2246c89ad5f5acb09ee
josegazano732 <josegazano7@outlook.com>
2022-04-16 02:18:02 -0300

    Se crearon modelos JLG

---
+++ SpringBoot/src/main/java/com/ejemplo/SpringBoot/model/ExperienciasModel.java
@@ -0,0 +1,81 @@
+
+package com.ejemplo.SpringBoot.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author JLG
+ */
+@Entity
+@Table(name="experiencia_laboral")
+public class ExperienciasModel implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nombreempresa;
+    private String fechainicio;
+    private String fechafin;
+    private String descripcion;
+    private String tipo_empleo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombreempresa() {
+        return nombreempresa;
+    }
+
+    public void setNombreempresa(String nombreempresa) {
+        this.nombreempresa = nombreempresa;
+    }
+
+    public String getFechainicio() {
+        return fechainicio;
+    }
+
+    public void setFechainicio(String fechainicio) {
+        this.fechainicio = fechainicio;
+    }
+
+    public String getFechafin() {
+        return fechafin;
+    }
+
+    public void setFechafin(String fechafin) {
+        this.fechafin = fechafin;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipo_empleo() {
+        return tipo_empleo;
+    }
+
+    public void setTipo_empleo(String tipo_empleo) {
+        this.tipo_empleo = tipo_empleo;
+    }
+    
+    
+
+ 
+    
+}
